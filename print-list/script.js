e5e2665b07620e025a348fbe2c64288a49d30559
Satsu05 <fleurdalia05@hotmail.com>
2022-02-01 14:56:18 +0100

    printlist

---
+++ print-list/script.js
@@ -0,0 +1,8 @@
+
+var artiest1 = { naam: "Hwasa", leeftijd: "26jaar", MBTItype: "ENFP" }
+var artiest2 = { naam: "Wheein", leeftijd: "26jaar", MBTItype: "INTP" }
+var artiest3 = { naam: "Moonbyul", leeftijd: "29jaar", MBTItype: "ENFP" }
+var artiest4 = { naam: "Solar", leeftijd: "30jaar", MBTItype: "INFJ" }
+var lijstObjecten = [artiest1, artiest2, artiest3, artiest4]
+
+for (let object of lijstObjecten) { console.log(object) }
