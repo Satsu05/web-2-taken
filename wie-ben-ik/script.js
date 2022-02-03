f1c8ecfbe1c98e516f00769474d544337e5eec01
Satsu05 <fleurdalia05@hotmail.com>
2022-02-03 16:36:45 +0100

    wie ben ik update

--- wie-ben-ik/script.js
+++ wie-ben-ik/script.js
@@ -2,4 +2,12 @@
 console.log("Dalia oefening werkt");
 let nElement = document.querySelector(".eerste-rij");
 console.log(nElement);
-nElement.style.backgroundColor = "blue"
+nElement.style.backgroundColor = "blue";
+
+let rij = "derde-rij";
+let list = document.getElementsByClassName(rij);
+console.log(list);
+//for (let list of list) {list.style.backgroundColor = "red"};
+for (let element of list) {
+    console.log(element);
+element.style.backgroundColor = "red"}
