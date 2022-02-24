86506f431f530b5ddbd6d636059b1fb035ae8c73
Satsu05 <fleurdalia05@hotmail.com>
2022-02-24 10:56:30 +0100

    r

--- print-list/script.js
+++ print-list/script.js
@@ -6,3 +6,11 @@ var artiest4 = { naam: "Solar", leeftijd: "30jaar", MBTItype: "INFJ" }
 var lijstObjecten = [artiest1, artiest2, artiest3, artiest4]
 
 for (let object of lijstObjecten) { console.log(object) }
+
+const namen = ["Dirk", "Sandy", "Tormund", "Dahlia"];
+
+for (let i = 0; i < namen.length; i++) {
+    // Gebruik de teller i als index van de array
+    // Elke iteratie wordt er dus een nieuw element aangesproken
+    console.log(namen[i]);
+}
